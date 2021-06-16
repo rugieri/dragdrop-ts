b2e7ae57b560d094eff81473af9ebec93b9d1221
rugieri <rugieri.ronaldo@gmail.com>
2021-06-15 22:54:04 -0300

    Added autobind decorator

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,20 @@
+//autobind decorator
+const autobind = (
+  target: any, 
+  methodName: string, 
+  descriptor: PropertyDescriptor
+  ) => {
+    const originalMethod = descriptor.value;
+    const adjDescriptor: PropertyDescriptor = {
+      configurable: true,
+      get() {
+        const boundFn = originalMethod.bind(this);
+        return boundFn;
+      }
+    };
+    return adjDescriptor;
+  }
+// ProjectInput Class
 class ProjectInput {
   templateElement: HTMLTemplateElement;
   hostElement: HTMLDivElement;
@@ -26,13 +43,15 @@ class ProjectInput {
     this.configure();
     this.attach();
   }
+
+  @autobind
   private submitHandler(event: Event) {
     event.preventDefault();
     console.log(this.titleInputElement.value);
   }
 
   private configure() {
-    this.element.addEventListener('submit', this.submitHandler.bind(this))
+    this.element.addEventListener('submit', this.submitHandler);
   }
 
   private attach() {
