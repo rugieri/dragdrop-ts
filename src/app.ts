523653d2e07d458cf3881bee20f85f54fabb0df3
rugieri <rugieri.ronaldo@gmail.com>
2021-06-29 23:03:29 -0300

    drag events now are reflecting the current state in the UI

--- src/app.ts
+++ src/app.ts
@@ -187,7 +187,7 @@ class ProjectItem extends Component<HTMLUListElement, HTMLLIElement>
     this.configure();
     this.renderContent();
   }
-  dragEndhandler(event: DragEvent): void {
+  dragEndhandler(_: DragEvent): void {
     throw new Error("Method not implemented.");
   }
   @autobind
@@ -212,7 +212,8 @@ class ProjectItem extends Component<HTMLUListElement, HTMLLIElement>
 }
 
 // ProjectList Class
-class ProjectList extends Component<HTMLDivElement, HTMLElement> {
+class ProjectList extends Component<HTMLDivElement, HTMLElement>
+  implements DragTarget {
   assignedProjects: Project[];
 
   constructor(private type: 'active' | 'finished') {
@@ -222,8 +223,25 @@ class ProjectList extends Component<HTMLDivElement, HTMLElement> {
     this.configure();
     this.renderContent();
   }
+  @autobind
+  dragOverHandler(_: DragEvent) {
+    const listEl = this.element.querySelector('ul')!;
+    listEl.classList.add('droppable');
+  }
+
+  dropHandler(_: DragEvent) {}
+  
+  @autobind
+  dragLeaveHandler(_: DragEvent) {
+    const listEl = this.element.querySelector('ul')!;
+    listEl.classList.remove('droppable');
+  }
 
   configure() {
+    this.element.addEventListener('dragover', this.dragOverHandler);
+    this.element.addEventListener('dragleave', this.dragLeaveHandler);
+    this.element.addEventListener('drop', this.dropHandler);
+
     projectState.addListener((projects: Project[]) => {
       const relevantProjects = projects.filter(prj => {
         if (this.type === 'active') {
