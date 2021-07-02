14eee5d0a25c75c5dfe9a8db2ba5f3b00d89c0a5
rugieri <rugieri.ronaldo@gmail.com>
2021-07-01 23:05:33 -0300

    added a droppable area

--- src/app.ts
+++ src/app.ts
@@ -190,9 +190,12 @@ class ProjectItem extends Component<HTMLUListElement, HTMLLIElement>
   dragEndhandler(_: DragEvent): void {
     throw new Error("Method not implemented.");
   }
+ 
+  
   @autobind
    dragStartHandler(event: DragEvent) {
-     console.log(event);
+     event.dataTransfer!.setData('text/plain', this.project.id);
+     event.dataTransfer!.effectAllowed = 'move';
    }
    
    dragEndHandler(_: DragEvent) {
@@ -206,7 +209,7 @@ class ProjectItem extends Component<HTMLUListElement, HTMLLIElement>
   
   renderContent() {
     this.element.querySelector('h2')!.textContent = this.project.title;
-    this.element.querySelector('h3')!.textContent = this.persons + 'assigned';
+    this.element.querySelector('h3')!.textContent = this.persons + ' assigned';
     this.element.querySelector('p')!.textContent = this.project.description;
   }
 }
@@ -224,15 +227,21 @@ class ProjectList extends Component<HTMLDivElement, HTMLElement>
     this.renderContent();
   }
   @autobind
-  dragOverHandler(_: DragEvent) {
-    const listEl = this.element.querySelector('ul')!;
-    listEl.classList.add('droppable');
+  dragOverHandler(event: DragEvent) {
+    if (event.dataTransfer && event.dataTransfer.types[0] === 'text/plain'){
+      event.preventDefault();
+      const listEl = this.element.querySelector('ul')!;
+      listEl.classList.add('droppable');
+    }
+    
   }
 
-  dropHandler(_: DragEvent) {}
+  dropHandler(event: DragEvent) {
+    console.log(event.dataTransfer!.getData('text/plain'));
+  }
   
   @autobind
-  dragLeaveHandler(_: DragEvent) {
+  dragLeaveHandler(_: DragEvent) {    
     const listEl = this.element.querySelector('ul')!;
     listEl.classList.remove('droppable');
   }
