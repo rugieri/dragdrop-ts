d9a9add2351719c251d255f00c5af519437f98bd
rugieri <rugieri.ronaldo@gmail.com>
2021-07-03 18:41:44 -0300

    now projects can be dragged from  "active" box  and dropped on "finished" box and vice-versa

--- src/app.ts
+++ src/app.ts
@@ -63,8 +63,19 @@ class ProjectState extends State<Project> {
       ProjectStatus.Active
     );
     this.projects.push(newProject);
+    this.updateListeners();
+  }
+  moveProject(projectId: string, newStatus: ProjectStatus) {
+    const project = this.projects.find(prj => prj.id === projectId);
+    if (project && project.status !== newStatus) {
+      project.status = newStatus;
+      this.updateListeners();
+    }
+  }
+
+  private updateListeners() {
     for (const listenerFn of this.listeners) {
-      listenerFn(this.projects.slice());
+    listenerFn(this.projects.slice());
     }
   }
 }
@@ -235,9 +246,10 @@ class ProjectList extends Component<HTMLDivElement, HTMLElement>
     }
     
   }
-
+  @autobind
   dropHandler(event: DragEvent) {
-    console.log(event.dataTransfer!.getData('text/plain'));
+    const prjId = event.dataTransfer!.getData('text/plain');
+    projectState.moveProject(prjId, this.type === 'active' ? ProjectStatus.Active : ProjectStatus.Finished);
   }
   
   @autobind
