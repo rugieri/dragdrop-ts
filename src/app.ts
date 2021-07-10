e6c2d5699d7dd6256a4db13496eae8e5c1959c93
rugieri <rugieri.ronaldo@gmail.com>
2021-07-10 15:43:29 -0300

    solved problems with namespace imports

--- src/app.ts
+++ src/app.ts
@@ -1,24 +1,8 @@
-/// <reference path="models/drag-drop.ts" />
-/// <reference path="models/project.ts" />
-/// <reference path="state/project-state.ts"/>
-/// <reference path="util/validation.ts"/>
-/// <reference path="decorators/autobind.ts"/>
-/// <reference path="components/base-component.ts"/>
-/// <reference path="components/project-item.ts"/>
 /// <reference path="components/project-list.ts"/>
 /// <reference path="components/project-input.ts"/>
-
-
 namespace App {
   new ProjectInput();
   new ProjectList('active');
   new ProjectList('finished');
-
-
-
-
-
-
-
 }
 
