c39c7417b2f72f7bbf9f1727b2e8934612ad2f0c
rugieri <rugieri.ronaldo@gmail.com>
2021-10-12 17:20:59 -0300

    Specify module resolution strategy: 'node'

--- src/components/project-item.ts
+++ src/components/project-item.ts
@@ -1,18 +1,20 @@
 import { Draggable } from '../models/drag-drop.js';
 import { Project } from '../models/project.js';
-import { Component } from './base-component.js';
+import Component from './base-component.js';
 import { autobind } from '../decorators/autobind.js';
 
-    //ProjectItem Class
-  export class ProjectItem extends Component<HTMLUListElement, HTMLLIElement>
-  implements Draggable {
+//ProjectItem Class
+export class ProjectItem
+  extends Component<HTMLUListElement, HTMLLIElement>
+  implements Draggable
+{
   private project: Project;
 
   get persons() {
     if (this.project.people === 1) {
       return '1 person';
     } else {
-      return `${this.project.people} persons` ;
+      return `${this.project.people} persons`;
     }
   }
 
@@ -24,29 +26,27 @@ import { autobind } from '../decorators/autobind.js';
     this.renderContent();
   }
   dragEndhandler(_: DragEvent): void {
-    throw new Error("Method not implemented.");
+    throw new Error('Method not implemented.');
   }
 
-  
   @autobind
-    dragStartHandler(event: DragEvent) {
-      event.dataTransfer!.setData('text/plain', this.project.id);
-      event.dataTransfer!.effectAllowed = 'move';
-    }
-    
-    dragEndHandler(_: DragEvent) {
-      console.log('DragEnd');
-    }
+  dragStartHandler(event: DragEvent) {
+    event.dataTransfer!.setData('text/plain', this.project.id);
+    event.dataTransfer!.effectAllowed = 'move';
+  }
+
+  dragEndHandler(_: DragEvent) {
+    console.log('DragEnd');
+  }
 
   configure() {
     this.element.addEventListener('dragstart', this.dragStartHandler);
     this.element.addEventListener('dragend', this.dragEndHandler);
   }
-  
+
   renderContent() {
     this.element.querySelector('h2')!.textContent = this.project.title;
     this.element.querySelector('h3')!.textContent = this.persons + ' assigned';
     this.element.querySelector('p')!.textContent = this.project.description;
   }
-  }
-
+}
